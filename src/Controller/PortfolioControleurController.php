089ab022cb02961149e9efff3f92bcbc771b8fcc
Lubin-vray <lubin.vray@etu.univ-st-etienne.fr>
2025-11-13 19:06:31 +0100

    j'en ai marre

--- src/Controller/PortfolioControleurController.php
+++ src/Controller/PortfolioControleurController.php
@@ -8,11 +8,34 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class PortfolioControleurController extends AbstractController
 {
-    #[Route('', name: 'app_portfolio_controleur')]
-    public function index(): Response
+    #[Route('/', name: 'portfolio_home')]
+    public function home(): Response
     {
         return $this->render('portfolio_controleur/index.html.twig', [
             'controller_name' => 'PortfolioControleurController',
         ]);
     }
+    #[Route('/loisirs', name: 'portfolio_loisirs')]
+    public function index(): Response
+    {
+        return $this->render('portfolio_controleur/loisirs.html.twig', [
+            'controller_name' => 'PortfolioControleurController',
+        ]);
+    }
+    #[Route('/competences', name: 'portfolio_competences')]
+    public function competences(): Response
+    {
+        return $this->render('portfolio_controleur/competences.html.twig', [
+            'controller_name' => 'PortfolioControleurController',
+        ]);
+    }
+    #[Route('/contact', name: 'portfolio_contact')]
+    public function contact(): Response
+    {
+        return $this->render('portfolio_controleur/contact.html.twig', [
+            'controller_name' => 'PortfolioControleurController',
+        ]);
+    }
+    
+   
 }
